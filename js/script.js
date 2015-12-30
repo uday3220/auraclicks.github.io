eeb0513a3b6d69872f5077f9ab2669d7eb6f4dd8
uday3220 <uday.3220@gmail.com>
2015-12-30 22:50:03 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@
 	// Thus the launchDay below denotes 7 May, 2014.
 	
 var newYear = new Date(); 
-newYear = new Date(2015, 12-1, 31);
+newYear = new Date(2016, 12-1, 1);
 $('.defaultCountdown').countdown({until: newYear, format: 'DHMS'}); 
 
 
